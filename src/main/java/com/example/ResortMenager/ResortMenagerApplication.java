a856d87ea10721985e79bed04c333b3be104f980
Filip Stach <filip.stach00@gmail.com>
2023-05-23 14:04:27 +0200

    first commit

---
+++ src/main/java/com/example/ResortMenager/ResortMenagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResortMenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortMenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResortMenagerApplication.class, args);
+	}
+
+}
